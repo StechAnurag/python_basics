51428dd661e7b0aee796e8c231ab5d21d6ad7049
Anurag Shrivastava <anurag.dev443@gmail.com>
2021-02-14 20:42:00 +0530

    boolean and type conversion

---
+++ 11_boolean.py
@@ -0,0 +1,11 @@
+#Boolean
+
+name = 'Anil Kapoor'
+
+is_cool = True
+
+is_cool = False
+
+print(bool(0))
+print(bool(1))
+print(bool('True'))
---
+++ exercises/02_type_conversion_ex.py
@@ -0,0 +1,7 @@
+# Make an Age Calculator
+
+birth_year = input('What\'s your birth year?')
+
+age = 2021 - int(birth_year)
+
+print(f'You\'re {age} years old.')
